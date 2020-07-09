044a63ae3702c1ea6e123883cc230a6a2dbe3809
Sam Armson <armson11@hotmail.co.uk>
2020-07-09 15:35:47 +0100

    Testing function calling
    Checking how MATLAB calls functions from different paths.

---
+++ functions/package_importer.m
@@ -0,0 +1,6 @@
+function package_importer
+	addpath 'C:/Users/Sam/My Documents/MATLAB/Colormaps/Colormaps (5)/Colormaps'; %Adds matplotlib colormaps
+	addpath 'C:/Users/Sam/My Documents/MATLAB/subtightplot/subtightplot'; %Adds package for nicer plotting
+	addpath 'C:/Users/Sam/My Documents/MATLAB/altmany-export_fig-3175417'; %Adds package for better figure exporting
+	startup_mtex
+end
---
+++ functions/test_func.m
@@ -0,0 +1,3 @@
+function test_func
+	disp("It's worked")
+end
