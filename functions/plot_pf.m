5293270f72c1e71a4fd315008ef8aee36e3017d0
Sam Armson <armson11@hotmail.co.uk>
2021-01-04 16:55:51 +0000

    Update plot_pf.m

--- functions/plot_pf.m
+++ functions/plot_pf.m
@@ -60,7 +60,7 @@ function pf = plot_pf(data_in,desired_pfs,varargin)
 	miller_indices = multi_miller(p.Results.desired_pfs)
 
 	if isa(data_in,'EBSD') == 1
-		if strcmp(p.Results.plot_type,'scatter') == 1 || strcmp(p.Results.plot_type,'none') == 1
+		if strcmp(p.Results.plot_type,'scatter') == 1
 			plotPDF(data_in(p.Results.phase_name).orientations,angle(data_in(p.Results.phase_name).orientations,p.Results.ref_text_comp)./degree,miller_indices,'antipodal','MarkerSize',p.Results.marker_size,'all','grid','grid_res',p.Results.grid_spacing*degree,'projection',p.Results.proj)
 		
 		elseif strcmp(p.Results.plot_type,'ODF') == 1
