0befc173f8daa7b23045e03d5269c4f3e26132dc
Sam Armson <armson11@hotmail.co.uk>
2021-02-26 17:49:28 +0000

    Update plot_map.m

--- functions/plot_map.m
+++ functions/plot_map.m
@@ -74,11 +74,28 @@ function pm = plot_map(data_in,map_type,varargin)
 		mapcolor = oM.orientation2color(data_in(phase_of_interest).orientations);
 	end
 
+
+
 	map_figure = figure('Name','Map loading...');
 	newMtexFigure(map_figure)
 
 	if isa(data_in,'EBSD') == 1
-		plot(data_in(p.Results.phase_name),mapcolor,'add2all');
+
+		if strcmp(map_type,'Deviation') == 1
+			plot(data_in(p.Results.phase_name),angle(data_in(phase_of_interest).orientations,p.Results.ref_text_comp)./degree))
+			colormap(gca,Parula);
+			Scale_bar_limits = [0 90]
+			caxis(Scale_bar_limits)
+			caxis(Scale_bar_limits);
+  			cb_new = mtexColorbar
+  			cb_new.Label.Interpreter = 'latex';
+  			set(cb_new,'TickLabelInterpreter', 'latex')
+		else
+			plot(data_in(p.Results.phase_name),mapcolor,'add2all');
+  		end
+
+
+
   		set(gca,'Color','black');
   		set(gcf, 'InvertHardcopy', 'off');
   		set(gca,'linewidth',3);
