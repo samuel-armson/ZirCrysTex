486a5f0e9fa2fb9ff254078fbcae14df12acd0ef
Sam Armson <armson11@hotmail.co.uk>
2021-02-28 11:06:49 +0000

    Update main_CSEBSD_test.m

--- main_CSEBSD_test.m
+++ main_CSEBSD_test.m
@@ -55,9 +55,9 @@ fibre_comp = define_fibre(reference_texture_component,cs)
 desired_pole_figures = [[0,0,0,2,"plane"];[1,-1,0,0,"plane"];[1,1,-2,0,"direction"]];
 plot_pf(ebsd,desired_pole_figures,'colouring','black')
 
-grains1 = create_grains(ebsd,'misorientation',10,'smallest_grain',3,'smoothing',10,'fill_gaps','yes')
-grains2 = create_grains(ebsd,'misorientation',10,'smallest_grain',5,'smoothing',10,'fill_gaps','yes')
-grains3 = create_grains(ebsd,'misorientation',10,'smallest_grain',10,'smoothing',10,'fill_gaps','yes')
+grains1 = create_grains(ebsd,'misorientation',10,'smallest_grain',3,'smoothing',3,'fill_gaps','yes')
+grains2 = create_grains(ebsd,'misorientation',10,'smallest_grain',3,'smoothing',5,'fill_gaps','yes')
+grains3 = create_grains(ebsd,'misorientation',10,'smallest_grain',3,'smoothing',10,'fill_gaps','yes')
 
 plot_map(ebsd,'IPF','plot_key','off')
 plot_map(grains1,'IPF','plot_key','off')
