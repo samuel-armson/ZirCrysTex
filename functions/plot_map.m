edca2c2c6c7c35222b0b677f25d098ea8ae716c8
Sam Armson <armson11@hotmail.co.uk>
2021-03-01 17:03:33 +0000

    Update plot_map.m

--- functions/plot_map.m
+++ functions/plot_map.m
@@ -162,7 +162,7 @@ function pm = plot_map(data_in,map_type,varargin)
   			unitcell_overlay_ori_data = data_in(phase_of_interest)
   			if strcmp(p.Results.view_unit_cell, 'CS') == 1
   				cross_section_correction = rotation('axis',xvector,'angle',-90*degree);
-  				unitcell_overlay_ori_data = rotate(unitcell_overlay_ori_data,cross_section_correction,'keepXY');
+  				[unitcell_overlay_ori_data, weird_other] = rotate(unitcell_overlay_ori_data,cross_section_correction,'keepXY');
   			end
   			crystal_diagram = crystalShape.hex(cs)
     		crystal_diagram_grains = unitcell_overlay_ori_data.meanOrientation * crystal_diagram * 0.5 * sqrt(unitcell_overlay_ori_data.area);
