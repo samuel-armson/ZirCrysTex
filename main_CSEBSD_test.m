98fb4936688abf75071d473ac05245252f6cecc0
Sam Armson <armson11@hotmail.co.uk>
2021-03-04 10:47:37 +0000

    Update main_CSEBSD_test.m

--- main_CSEBSD_test.m
+++ main_CSEBSD_test.m
@@ -60,6 +60,7 @@ grains1 = create_grains(ebsd,'misorientation',10,'smallest_grain',3,'smoothing',
 plot_map(ebsd,'BC')
 plot_map(ebsd,'IPF','plot_key','off')
 plot_map(grains1,'Deviation','view_unit_cell','CS')
+plot_map(grains1,'Deviation')
 
 grain_area_hist(grains1,'bin_size',1,'max_size',50,'units','um')
 grain_dimension_hist(grains1,'bin_size',1,'max_size',25,'units','um')
