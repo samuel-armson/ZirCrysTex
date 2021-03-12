a24fa038fc0425ac6c5d0e69951fd8df90ba7194
Sam Armson <armson11@hotmail.co.uk>
2021-03-12 09:05:17 +0000

    Update main_SPED_test.m

--- main_SPED_test.m
+++ main_SPED_test.m
@@ -51,7 +51,7 @@ cs = ebsd(phase_of_interest).CS
 
 % Perform cross-section correction
 %ebsd = x_section_correction(ebsd,'SPED','scan_rotation',90)
-ebsd = dataset_rotation(ebsd_out,[0,0,90],'axis');
+ebsd = dataset_rotation(ebsd,[0,0,90],'axis');
 
 fibre_comp = define_fibre(reference_texture_component,cs)
 
