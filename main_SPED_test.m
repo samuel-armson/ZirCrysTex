198ab68c468ff170c0c4210f2c3a2162adc56a0c
Sam Armson <armson11@hotmail.co.uk>
2021-03-14 12:45:24 +0000

    Update main_SPED_test.m

--- main_SPED_test.m
+++ main_SPED_test.m
@@ -57,7 +57,7 @@ ebsd = x_section_correction(ebsd,'SPED','scan_rotation',90)
 
 %fibre_comp = define_fibre(reference_texture_component,cs)
 
-%grains1 = create_grains(ebsd,'misorientation',10,'smallest_grain',1,'smoothing',1,'fill_gaps','no')
+grains1 = create_grains(ebsd,'misorientation',10,'smallest_grain',1,'smoothing',1,'fill_gaps','no')
 %odf = make_ODF(ebsd)
 odf_data= calcODF(ebsd(phase_of_interest).orientations,'halfwidth', 3*degree)
 
@@ -68,10 +68,10 @@ plot_pf(odf_data,desired_pole_figures)
 
 %plot_map(ebsd,'BC')
 %plot_map(ebsd,'IPF','plot_key','off')
-%plot_map(grains1,'Deviation')
+plot_map(grains1,'Deviation')
 plot_map(ebsd,'Deviation','plot_key','off')
 %plot_map(ebsd,'phase')
-%plot_map(grains1,'phase')
+plot_map(grains1,'phase')
 
 %grain_area_hist(grains1,'bin_size',1,'max_size',50,'units','nm')
 %grain_dimension_hist(grains1,'bin_size',1,'max_size',25,'units','nm')
