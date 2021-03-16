d43a647545d2894fb18a7660ddd38064162ecf54
Sam Armson <armson11@hotmail.co.uk>
2021-03-16 10:28:24 +0000

    Update main_SPED_test.m

--- main_SPED_test.m
+++ main_SPED_test.m
@@ -20,8 +20,9 @@ save_figures = 'no';
 Sample_ID = "2NVa3";
 % Path to files. eg: 'J:/Nature Paper Figures/'
 pname = 'D:/Sam/Dropbox (The University of Manchester)/Sam Armson shared folder/Experimental/SPED/MIBL EX HIGH DR 3/';
+
 % File name with pname prefix, eg: [pname 'SPED_Substrate_MARIA.ctf']
-fname_full = [pname 'MIBL EX HIGH DR 3 R_1.6_EE_0.4_amb_res_ir_350_crystal.ctf'];
+fname_full = [pname 'MIBL EX HIGH DR 3 R_1.6_EE_0.4_more_phases_index400_or_1.ctf'];
 
 % Phase of interest for orientation analysis - select here for global phase of interest.
 
@@ -33,12 +34,16 @@ reference_texture_component = [1,0,-3];
 
 % UPDATE THIS ACCORDING TO YOUR CTF FILE.
 % crystal symmetry
-CS = {...
-	'notIndexed',...
-	crystalSymmetry('12/m1', [5.169 5.232 5.341], [90,99,90]*degree, 'X||a*', 'Y||b*', 'Z||c', 'mineral', 'Monoclinic ZrO$$_2$$', 'color', [34, 111, 84]/255),...
-	crystalSymmetry('6/mmm', [3.232 3.232 5.147], 'X||a*', 'Y||b', 'Z||c*', 'mineral', 'HCP Zr', 'color', [2, 123, 206]/255),...
-	crystalSymmetry('6/mmm', [5.312 5.312 3.197], 'X||a*', 'Y||b', 'Z||c*', 'mineral', 'H-Suboxide', 'color', [227, 181, 5]/255),...
-	crystalSymmetry('4/mmm', [3.596 3.596 5.184], 'mineral', 'Tetragonal ZrO$$_2$$', 'color', [160, 35, 41]/255)};
+CS = {... 
+  'notIndexed',...
+  crystalSymmetry('12/m1', [5.169 5.232 5.341], [90,99,90]*degree, 'X||a*', 'Y||b*', 'Z||c', 'mineral', 'Monoclinic ZrO$$_2$$', 'color', 'dark green'),...
+  crystalSymmetry('6/mmm', [5.312 5.312 3.197], 'X||a*', 'Y||b', 'Z||c*', 'mineral', 'Hexagonal ZrO', 'color', 'yellow'),...
+  crystalSymmetry('4/mmm', [3.596 3.596 5.184], 'mineral', 'Tetragonal ZrO$$_2$$', 'color', 'dark red'),...
+  crystalSymmetry('6/mmm', [3.232 3.232 5.147], 'X||a*', 'Y||b', 'Z||c*', 'mineral', 'HCP Zr', 'color', 'dark blue'),...
+  crystalSymmetry('4/mmm', [3.52 3.52 4.45], 'mineral', 'Zr Hydride', 'color', 'magenta'),...
+  crystalSymmetry('-3m1', [5.038 5.038 13.772], 'X||a*', 'Y||b', 'Z||c*', 'mineral', 'Hematite', 'color', 'cyan'),...
+  crystalSymmetry('m-3m', [4.086 4.086 4.086], 'mineral', 'Amorphous Pt', 'color', 'magenta')};
+
 
 
   
@@ -59,22 +64,22 @@ ebsd = x_section_correction(ebsd,'SPED','scan_rotation',90)
 
 grains1 = create_grains(ebsd,'misorientation',10,'smallest_grain',2,'smoothing',1,'fill_gaps','no')
 %odf = make_ODF(ebsd)
-odf_data= calcODF(ebsd(phase_of_interest).orientations,'halfwidth', 3*degree)
+%odf_data= calcODF(ebsd(phase_of_interest).orientations,'halfwidth', 3*degree)
 
 %desired_pole_figures = [[1,0,-3,"plane"];[1,0,-4,"plane"];[1,0,-5,"plane"];[1,0,-6,"plane"]];
 
 %plot_pf(ebsd,desired_pole_figures)
 %plot_pf(odf_data,desired_pole_figures)
 
-%plot_map(ebsd,'BC')
+plot_map(ebsd,'BC')
 %plot_map(ebsd,'IPF','plot_key','off')
-plot_map(grains1,'Deviation')
+%plot_map(grains1,'Deviation')
 %plot_map(ebsd,'Deviation','plot_key','off')
-%plot_map(ebsd,'phase')
-%plot_map(grains1,'phase')
+plot_map(ebsd,'phase')
+plot_map(grains1,'phase')
 
 %grain_area_hist(grains1,'bin_size',1,'max_size',50,'units','nm')
-grain_dimension_hist(grains1,'bin_size',0.001,'max_size',0.03,'units','nm','max_percentage',6)
+%grain_dimension_hist(grains1,'bin_size',0.001,'max_size',0.03,'units','nm','max_percentage',6)
 
 
 %% Sign off
