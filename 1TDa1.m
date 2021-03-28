b720f95e1caf0e95455ac1ead28e6f328d46fc13
Sam Armson <armson11@hotmail.co.uk>
2021-03-28 19:31:54 +0100

    Corrected

--- 1TDa1.m
+++ 1TDa1.m
@@ -59,7 +59,7 @@ grains_mono = create_grains(ebsd_mono,'misorientation',10,'smallest_grain',2,'sm
 grains_met = create_grains(ebsd_met,'misorientation',10,'smallest_grain',5,'smoothing',5,'fill_gaps','no','phase_name','HCP Zr')
 
 
-odf = make_ODF(ebsd_mono)
+odf = make_ODF(ebsd_mono('Monoclinic ZrO$$_2$$'))
 odf_data= calcODF(ebsd_mono('Monoclinic ZrO$$_2$$').orientations,'halfwidth', 3*degree)
 desired_pole_figures = [[1,0,-3,"plane"];[1,0,-4,"plane"];[1,0,-5,"plane"];[1,0,-6,"plane"]];
 plot_pf(ebsd_mono,desired_pole_figures)
