aa177c4cc3f5d9e91b7692c5978d78e053ba0c40
samuel-armson <armson11@hotmail.co.uk>
2021-03-29 19:12:57 +0100

    Update J_1TDa1.m

--- J_1TDa1.m
+++ J_1TDa1.m
@@ -2,6 +2,7 @@
 
 % Select mTeX installation to use and start mTeX
 %addpath 'C:/Users/Rhys/My Documents/MATLAB/mtex-5.1.1';
+addpath 'C:/Users/Sam/Documents/GitHub/Zirconia_mTeX'
 addpath 'functions';
 addpath 'third_party_packages';
 % Define global variables
@@ -10,7 +11,7 @@ global cs
 global reference_texture_component
 global Sample_ID
 global pname
-addpath 'C:/Users/Sam/My Documents/MATLAB/mtex-5.6.1';
+addpath 'C:/Users/Sam/My Documents/MATLAB/mtex-5.6.1/mtex-5.6.1';
 startup_mtex
 
 % Saving figures takes time. Best to only use on final run: 'on' or 'no'. Apply to all functions here.
