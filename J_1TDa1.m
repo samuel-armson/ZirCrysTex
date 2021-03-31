eccbe40eb0ccd0da6ccfa0d196263551430397af
Sam Armson <armson11@hotmail.co.uk>
2021-03-31 10:47:51 +0100

    Update J_1TDa1.m

--- J_1TDa1.m
+++ J_1TDa1.m
@@ -72,7 +72,9 @@ plot_map(ebsd_full,'phase')
 plot_map(grains_full,'phase')
 plot_map(grains_mono,'Deviation','phase_name','Monoclinic ZrO$$_2$$','ref_text_comp',[1,0,-3])
 plot_map(grains_met,'Deviation','phase_name','HCP Zr','crys_sym',ebsd_full('HCP Zr').CS,'ref_text_comp',[0,0,0,2],'view_unit_cell','CS')
+%%
 plot_substrate_mono(grains_met,grains_mono)
+%%
 %combine_figures(f2,f3)
 
 grain_dimension_hist_ellipse(grains_mono,'bin_size',5,'max_size',250,'units','nm','max_percentage',20)
