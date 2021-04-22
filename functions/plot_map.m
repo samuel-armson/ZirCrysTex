9869ca51d5d0ca3f817d6acb441c91feb8e63bf2
Sam Armson <armson11@hotmail.co.uk>
2021-04-22 19:02:44 +0100

    Update plot_map.m

--- functions/plot_map.m
+++ functions/plot_map.m
@@ -157,7 +157,7 @@ function pm = plot_map(data_in,map_type,varargin)
   				hold on
   				grain_data = p.Results.gb_overlay(p.Results.phase_name)
   				boundaries = grain_data.boundary
-  				boundaries_for_misorientation = boundaries(p.Results.phase_name,p.Results.phase_name)
+  				boundaries_for_misorientation = boundaries
   				%plot(boundaries,'lineColor',[190,25,25]./255)
   				plot(boundaries_for_misorientation,boundaries_for_misorientation.misorientation.angle./degree)
   				hold off
