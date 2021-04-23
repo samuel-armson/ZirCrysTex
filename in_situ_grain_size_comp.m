0f769758a9a837f0fe413a75f2d179dda28bdc90
Sam Armson <armson11@hotmail.co.uk>
2021-04-23 10:31:20 +0100

    Update in_situ_grain_size_comp.m

--- in_situ_grain_size_comp.m
+++ in_situ_grain_size_comp.m
@@ -97,7 +97,7 @@ for sgi = 1:length(param_list)
 	%grain_dimension_hist_ellipse(grainsets{1,sgi},'bin_size',5,'max_size',1000,'units','nm','max_percentage',10)
 	plot_map(param_list{1,sgi},'BC','gb_overlay',grainsets{1,sgi},'phase_name','Monoclinic ZrO$$_2$$')
 	plot_map(grainsets{1,sgi},'BC','phase_name','Monoclinic ZrO$$_2$$')
-	plot_map(grainsets{1,sgi},'gb_only','phase_name','Monoclinic ZrO$$_2$$')
+	plot_map(param_list{1,sgi},'gb_only','phase_name','Monoclinic ZrO$$_2$$')
 end
 
 %%
