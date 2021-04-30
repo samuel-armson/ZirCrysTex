b61205cec744809cc70c847f78a51fb39ac91182
Sam Armson <armson11@hotmail.co.uk>
2021-04-30 10:49:49 +0100

    Update shape_prefered_orientation.m

--- functions/shape_prefered_orientation.m
+++ functions/shape_prefered_orientation.m
@@ -45,7 +45,7 @@ function gdh = shape_prefered_orientation(data_in,varargin)
 	Fig_2d_hist_as_res= figure('Name','Loading...');
 	figure(Fig_2d_hist_as_res);
   w = data_in(p.Results.phase_name).area .* (data_in(p.Results.phase_name).aspectRatio-1);
-  histogram(omega,'BinCounts',p.Results.bin_quant, 'weights', w)
+  histogram(omega,'nbins',p.Results.bin_quant, 'weights', w)
   title(p.Results.titles)
   
 
