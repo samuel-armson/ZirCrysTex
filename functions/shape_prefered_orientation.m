38331e9875f12d80e064db820ee8df1450d534e9
Sam Armson <armson11@hotmail.co.uk>
2021-05-03 16:32:12 +0100

    Update shape_prefered_orientation.m

--- functions/shape_prefered_orientation.m
+++ functions/shape_prefered_orientation.m
@@ -46,10 +46,12 @@ function gdh = shape_prefered_orientation(data_in,varargin)
 	figure(Fig_2d_hist_as_res);
   w = data_in(p.Results.phase_name).area .* (data_in(p.Results.phase_name).aspectRatio-1);
   blank = ones(length(omega),1)
-  Options = {'anglenorth', 0, 'angleeast', 90,'ndirections',64,'labelnorth','Map y-xis','labeleast','90','labelwest','-90','labelsouth',''}
+  Options = {'anglenorth', 0, 'angleeast', 90,'ndirections',64,'labelnorth',...
+              'Map y-xis','labeleast',' 90','labelwest','-90','labelsouth','',...
+              'titlestring',p.Results.titles,'lablegend','Grain Size'}
   WindRose(omega,blank,Options)
   %histogram(data_in(p.Results.phase_name).longAxis,p.Results.bin_quant)
-  title(p.Results.titles)
+  %title(p.Results.titles)
   
 
 
