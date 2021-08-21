d20e87d0a954f313919de3c711b44cda4c463260
Sam Armson <armson11@hotmail.co.uk>
2021-08-21 16:56:44 +0100

    Update plot_crys_shape.m

--- functions/plot_crys_shape.m
+++ functions/plot_crys_shape.m
@@ -129,24 +129,21 @@ function pm = plot_crys_shape(data_in,grain_ids,varargin)
 		cross_section_correction = rotation('axis',xvector,'angle',270*degree);
 		crystal_diagram_grains = rotate(crystal_diagram_grains,cross_section_correction);
 		for grain_id = 1:length(p.Results.grain_ids)
-			%grain_figure = figure('Name',int2str(grain_ids(grain_id)));
-			%newMtexFigure(grain_figure)
-			
-			disp(grain_id)
-			disp(grain_ids(grain_id))
+			grain_figure = figure('Name',int2str(grain_ids(p.Results.grain_id)));
+			newMtexFigure(grain_figure)
 
-			%grain = crystal_diagram_grains(grain_ids(grain_id));
+			grain = crystal_diagram_grains(grain_ids(p.Results.grain_id));
 			
-			%plot(grain,'FaceColor',[200 200 200]/255,'FaceAlpha',0.8,'linewidth',1.5)
-			%set(gca,'DataAspectRatio',[1 1 1]);
-			%set(gca,'XColor', 'none','YColor','none');
-			%set(gcf,'color','none');
-			%set(gca,'color','none');
-			%title(int2str(grain_ids(grain_id)));
+			plot(grain,'FaceColor',[200 200 200]/255,'FaceAlpha',0.8,'linewidth',1.5)
+			set(gca,'DataAspectRatio',[1 1 1]);
+			set(gca,'XColor', 'none','YColor','none');
+			set(gcf,'color','none');
+			set(gca,'color','none');
+			title(int2str(grain_ids(p.Results.grain_id)));
 			
 			
-			%filename = strcat(output_loc,'UC_grain_',int2str(grain_ids(grain_id)),'.png')
-			%export_fig(filename, '-dpng', '-transparent', '-r600');
+			filename = strcat(output_loc,'UC_grain_',int2str(grain_ids(p.Resultsgrain_id)),'.png')
+			export_fig(filename, '-dpng', '-transparent', '-r600');
 		end
 		
 
