d4e0bbb1bc1f3b39ea821373cb02997b5d86a709
Sam Armson <armson11@hotmail.co.uk>
2021-12-14 20:10:07 +0000

    Update Slice_207_3D_EBSD.m

--- Slice_207_3D_EBSD.m
+++ Slice_207_3D_EBSD.m
@@ -89,11 +89,9 @@ grains_mis = smooth(grains_mis,3);
 
 bounds_1 = grains_1.boundary(phase_of_interest,phase_of_interest)
 
-plot_map(grains_1,'IPF','phase_name','HCP Zr','crys_sym',ebsd_1('HCP Zr').CS,'ref_text_comp',[0,0,0,2],'plot_key','on','ipf_key',ipfHSVKey(cs.Laue),'facealpha',0.5)
-
-hold on
+%plot_map(grains_1,'IPF','phase_name','HCP Zr','crys_sym',ebsd_1('HCP Zr').CS,'ref_text_comp',[0,0,0,2],'plot_key','on','ipf_key',ipfHSVKey(cs.Laue),'facealpha',0.5)
+figure()
 plot(bounds_1,bounds_1.misorientation.angle./degree,'linewidth',2)
-hold off
 mtexColorMap parula
 mtexColorbar('title','misorientation angle')
 
