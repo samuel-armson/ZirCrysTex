5206fcc7c835d1dc512165510941419449f32ab3
Sam Armson <armson11@hotmail.co.uk>
2021-12-14 12:53:50 +0000

    Update plot_pf.m

--- functions/plot_pf.m
+++ functions/plot_pf.m
@@ -134,8 +134,8 @@ function pf = plot_pf(data_in,desired_pfs,varargin)
 					end
     			end
 				titleString = strcat(titleString,"\right\}$$ plane-normal deviation from growth direction $$ \left(^{\circ}\right)$$")
-				colormap(parula_red('increment',1));
-				%colormap(plasma);
+				%colormap(parula_red('increment',1));
+				colormap(plasma);
 				cb = mtexColorbar('location','southoutside');
 				x_label = xlabel(cb, titleString,'FontSize',8)
 				Scale_bar_limits = [0 90]
