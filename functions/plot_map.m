d647b785a707d17ab12780ea4c9642a89da80a8a
Sam Armson <armson11@hotmail.co.uk>
2022-02-05 12:01:02 +0000

    Update plot_map.m

--- functions/plot_map.m
+++ functions/plot_map.m
@@ -203,7 +203,7 @@ function pm = plot_map(data_in,map_type,varargin)
 		elseif strcmp(map_type,'phase')
 			map_figure = figure('Name','Map loading...');
 			newMtexFigure(map_figure)
-			plot(data_in('indexed'));
+			plot(data_in('indexed'))
 			aspect_ratio_correction = 1.2
 			%hold on
     		%plot(data_in('notIndexed'),'FaceColor','black')
