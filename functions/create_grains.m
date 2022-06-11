5a448b6954e0dbc62a5f07a827cbf0efd9c0b92c
Sam Armson <armson11@hotmail.co.uk>
2022-06-11 13:02:41 +0100

    Update create_grains.m

--- functions/create_grains.m
+++ functions/create_grains.m
@@ -50,9 +50,8 @@ function gr = create_grains(data_in,desired_pfs,varargin)
   	end
 
   	disp('Cleaning grains...')
-  	%[grains_clean,ebsd_full(phase_of_interest).grainId] = calcGrains(ebsd_full(phase_of_interest),'angle',Grain_mis_param,'boundary','tight','unitCell');
+  	[grains_clean,ebsd_full(phase_of_interest).grainId] = calcGrains(ebsd_full(phase_of_interest),'angle',Grain_mis_param,'boundary','convexhull','unitCell');
   	%ebsd_phase_smoothed = smooth(ebsd_full(phase_of_interest),grains_dirty,splineFilter,'fill');
-  	grains_clean=grains_dirty
 
   	disp('Smooting grains...')
 	%grains_clean = smooth(grains_clean,Grain_smooth_param);
