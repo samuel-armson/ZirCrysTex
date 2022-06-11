b47c313bcf0c0651317729373e5d469ad7edb8e4
samuel-armson <41579229+samuel-armson@users.noreply.github.com>
2022-06-11 14:11:23 +0100

    Update J_1TDa1_no_LM_ASTAR_params.m

--- J_1TDa1_no_LM_ASTAR_params.m
+++ J_1TDa1_no_LM_ASTAR_params.m
@@ -57,7 +57,7 @@ ebsd_mono = x_section_correction(ebsd_mono,'SPED','scan_rotation',90)
 
 %ebsd_mono_01 = ebsd_mono(ebsd_mono.mad>0.01)
 %ebsd_mono_02 = ebsd_mono(ebsd_mono.mad>0.02)
-ebsd_mono_05 = ebsd_mono(ebsd_mono.mad>0.05)
+%ebsd_mono_05 = ebsd_mono(ebsd_mono.mad>0.05)
 %ebsd_mono_10 = ebsd_mono(ebsd_mono.mad>0.1)
 
 
@@ -65,7 +65,7 @@ ebsd_mono_05 = ebsd_mono(ebsd_mono.mad>0.05)
 grains_mono = create_grains(ebsd_mono,'misorientation',15,'smallest_grain',1,'smoothing',1,'fill_gaps','no','phase_name','Monoclinic ZrO$$_2$$')
 %grains_mono_01 = create_grains(ebsd_mono_01,'misorientation',15,'smallest_grain',1,'smoothing',1,'fill_gaps','no','phase_name','Monoclinic ZrO$$_2$$')
 %grains_mono_02 = create_grains(ebsd_mono_02,'misorientation',15,'smallest_grain',1,'smoothing',1,'fill_gaps','no','phase_name','Monoclinic ZrO$$_2$$')
-grains_mono_05 = create_grains(ebsd_mono_05,'misorientation',15,'smallest_grain',1,'smoothing',1,'fill_gaps','no','phase_name','Monoclinic ZrO$$_2$$')
+%grains_mono_05 = create_grains(ebsd_mono_05,'misorientation',15,'smallest_grain',1,'smoothing',1,'fill_gaps','no','phase_name','Monoclinic ZrO$$_2$$')
 %grains_mono_10 = create_grains(ebsd_mono_10,'misorientation',15,'smallest_grain',1,'smoothing',1,'fill_gaps','no','phase_name','Monoclinic ZrO$$_2$$')
 %grains_met = create_grains(ebsd_met,'misorientation',15,'smallest_grain',5,'smoothing',5,'fill_gaps','no','phase_name','HCP Zr')
 
@@ -83,14 +83,14 @@ grains_mono_05 = create_grains(ebsd_mono_05,'misorientation',15,'smallest_grain'
 
 
 %plot_map(ebsd_mono,'BC','gb_overlay',grains_mono,'phase_name','Monoclinic ZrO$$_2$$')
-%plot_map(ebsd_mono,'BC','phase_name','Monoclinic ZrO$$_2$$')
+plot_map(ebsd_mono,'BC','phase_name','Monoclinic ZrO$$_2$$')
 %plot_map(grains_mono,'gb_only','phase_name','Monoclinic ZrO$$_2$$')
 
 %plot_map(grains_full,'phase')
 plot_map(grains_mono,'Deviation','phase_name','Monoclinic ZrO$$_2$$','ref_text_comp',[1,0,-3])
 %plot_map(grains_mono_01,'Deviation','phase_name','Monoclinic ZrO$$_2$$','ref_text_comp',[1,0,-3])
 %plot_map(grains_mono_02,'Deviation','phase_name','Monoclinic ZrO$$_2$$','ref_text_comp',[1,0,-3])
-plot_map(grains_mono_05,'Deviation','phase_name','Monoclinic ZrO$$_2$$','ref_text_comp',[1,0,-3])
+%plot_map(grains_mono_05,'Deviation','phase_name','Monoclinic ZrO$$_2$$','ref_text_comp',[1,0,-3])
 %plot_map(grains_mono_10,'Deviation','phase_name','Monoclinic ZrO$$_2$$','ref_text_comp',[1,0,-3])
 %plot_map(grains_met,'Deviation','phase_name','HCP Zr','crys_sym',ebsd_full('HCP Zr').CS,'ref_text_comp',[0,0,0,2],'view_unit_cell','CS')
 
