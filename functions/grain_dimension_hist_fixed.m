01f2f79089d73fbc8bf49b5acdcedaa662172e2b
Sam Armson <armson11@hotmail.co.uk>
2023-01-30 12:05:53 +0000

    Update grain_dimension_hist_fixed.m

--- functions/grain_dimension_hist_fixed.m
+++ functions/grain_dimension_hist_fixed.m
@@ -96,8 +96,8 @@ function gdh = grain_dimension_hist_fixed(data_in,varargin)
 	%newxlabels = linspace(0,max(max_axis_val),length(min_axis_bins))
 	%yticks = get(gca,'ytick');
 	%newylabels = linspace(0,max(max_axis_val),length(min_axis_bins));
-	xticks(0:length(min_axis_bins):max(max_axis_val))
-	yticks(0:length(min_axis_bins):max(max_axis_val))
+	xticks(0:ax_bin_size:max_axis_val)
+	yticks(0:ax_bin_size:max_axis_val)
 	%set(gca,'xticklabel',newxlabels);
 	%set(gca,'yticklabel',newylabels);
 	set(gca,'TickDir','out');
